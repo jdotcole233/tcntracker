7b8c0f1c73c5d3ccab7ba66a1b332696eee10401
unknown <ebaidoo79@gmail.com>
2018-12-17 10:57:10 +0000

    Testing the USSD again 23

--- app/Http/Controllers/ussdController.php
+++ app/Http/Controllers/ussdController.php
@@ -25,8 +25,12 @@ class ussdController extends Controller
       if ($request->USERDATA != null){
         if ($request->USERDATA == "1"){
           $response_one = "Enter total weight";
-          session()->put("weight","filled");
+          session()->put("weight".$request->MSISDN,"true");
           return $this->data_tosend($request->MSISDN,$response_one,true);
+        } else if (($request->USERDATA == "1" && session()->has("weight".$request->MSISDN)){
+          $expected_payment = $this->ussd_price_compute($found_comm_price,$request->USERDATA);
+          session()->flush("weight".$request->MSISDN);
+          return $this->data_tosend($request->MSISDN,$expected_payment,false);
         }
         return $this->data_tosend($request->MSISDN,$this->ussd_output($found_name, $community_name.intval($request->USERDATA), $found_comm_price), true);
       } else{
