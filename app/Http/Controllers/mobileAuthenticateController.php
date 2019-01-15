026ae437f503710dc2ed94b4ecbd1c3cbf192cd1
jdotcole233 <jdotcole233@gmail.com>
2019-01-15 00:30:12 +0000

    working on mobile auth

--- app/Http/Controllers/mobileAuthenticateController.php
+++ app/Http/Controllers/mobileAuthenticateController.php
@@ -3,13 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class mobileAuthenticateController extends Controller
 {
 
     public function handle(Request $request){
 
+      $credentials = $request->only('email','password');
 
-        return response()->json("Welcome to this site");
+      if (Auth::attempt($credentials)){
+          $buyerID = Auth::user()->buyersbuyer_id;
+          if ($buyerDetails == null){
+            return;
+          }
+          $buyerDetails = DB::table('buyers')->join('buyer_communities','buyers.buyer_id','buyers_communities.buyer_id')->where('buyers_id', $buyerID)->first();
+          $total_farmers_registered = Buyer_farmer_registration::where('buyersbuyer_id', $buyerID)->count();
+          $total_weight_bought = Farmer_transaction::where('buyersbuyer_id', $buyerID)->sum('total_weight');
+          $current_price = Community_price::where('communitiescommunity_id', $buyerDetails->community_id)->latest()->value('current_price');
+
+        return response()->json($buyerDetails);
+      }
+
+        return response()->json("null");
     }
 }
